12fdb68c2a9187d4189396cfb98c8c3533cbe58d
nup-data <85929669+nup-data@users.noreply.github.com>
2021-06-29 14:56:31 +0200

    Quitar marco de iframe

--- streamlit_observable/frontend/src/Observable.tsx
+++ streamlit_observable/frontend/src/Observable.tsx
@@ -97,25 +97,9 @@ class Observable extends StreamlitComponentBase<{}> {
 
   public render = (): ReactNode => {
     return (
-      <div style={{ border: '1px solid gray', borderRadius: '4px' }}>
+      <div style={{ border: '0px solid gray', borderRadius: '4px' }}>
         <div style={{ padding: '9px 12px' }}>
           <div ref={this.notebookRef}></div>
-        </div>
-        <div style={{ marginTop: '4px' }}>
-          
-          <div style={{
-            backgroundColor: '#ddd',
-            fontWeight: 700,
-            padding: ".25rem .5rem",
-            borderRadius: '0 0 4px 4px',
-            gridTemplateColumns: "auto auto",
-            display:"grid"
-          }}>
-            <div style={{textAlign:"left"}}>{this.props.args.name}</div>
-            <div style={{textAlign:"right"}}>
-            <a href={`https://observablehq.com/${this.props.args.notebook}`} style={{ color: '#666', }}>{this.props.args.notebook}</a>
-            </div>
-          </div>
         </div>
       </div >
     )
